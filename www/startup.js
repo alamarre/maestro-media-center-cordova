614051e3de7bc51ad1a8d5bd6d401041510118f8
Al Lamarre <alamarre@gmail.com>
2018-12-24 10:09:45 -0500

    scale better

--- www/startup.js
+++ www/startup.js
@@ -1,21 +1,40 @@
 if(cordova) {
-  window.chromecastPromise = new Promise((s) => {
-    function checkForPlugin() {
-      if(cordova.plugins && cordova.plugins.chromecastios) {
-        s();
-      } else {
-        setTimeout(checkForPlugin, 100);
-      }
-    }
-    checkForPlugin();
-  })
+    window.chromecastPromise = new Promise((s) => {
+                                           function checkForPlugin() {
+                                           if(cordova.plugins && cordova.plugins.chromecastios) {
+                                           s();
+                                           } else {
+                                           setTimeout(checkForPlugin, 100);
+                                           }
+                                           }
+                                           checkForPlugin();
+                                           })
 } else {
-  window.chromecastPromise = new Promise(() => {});
+    window.chromecastPromise = new Promise(() => {});
 }
 
-  window.maestroSettings = {
-      "HOST": "gladiator.omny.ca",
-      "PORT": 443,
-      "PROTOCOL": "https",
-      "NEVER_HIDE_SETTINGS": true
-  };
+async function loadFromCache() {
+    const js = await localStorage.getItem("maestroJs");
+    attachJs(js);
+}
+
+function attachJs(js) {
+    let script = document.createElement("script");
+    script.innerHTML = js;
+    document.body.appendChild(script);
+}
+
+fetch("https://videos.omny.ca/app.js").then(response => {
+                                            return response.text();
+                                            }, loadFromCache)
+.then((js) => {
+      localStorage.setItem("maestroJs", js);
+      attachJs(js);
+      }, loadFromCache);
+
+window.maestroSettings = {
+    "HOST": "gladiator.omny.ca",
+    "PORT": 443,
+    "PROTOCOL": "https",
+    "NEVER_HIDE_SETTINGS": true
+};
